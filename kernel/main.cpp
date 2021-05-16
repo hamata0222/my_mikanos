8c03ca733df926ae655777e01b580edb4aede144
Hiroshi Hamada <jp.ne.hiroshi@gmail.com>
2021-05-16 15:01:04 +0900

    osbook_day05fまで写経

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -19,8 +19,24 @@ void* operator new(size_t size, void* buf) {
 void operator delete(void* obj) noexcept {
 }
 
-char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)]; // PixelWriteのインスタンスを配置する領域
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)]; // PixelWriterのインスタンスを配置する領域
 PixelWriter* pixel_writer;
+
+char console_buf[sizeof(Console)]; // Consoleのインスタンスを配置する領域
+Console* console;
+
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -40,12 +56,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     }
   }
 
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  char buf[128];
   for (int i = 0; i < 27; ++i){
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+    printk("printk: %d\n", i);
   }
 
   while (1) __asm__("hlt");
