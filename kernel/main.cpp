3032bc44596d0e8efb1b273a169f096410956196
Hiroshi Hamada <jp.ne.hiroshi@gmail.com>
2021-07-25 10:12:14 +0900

    osbook_day08cまで写経

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -29,6 +29,7 @@
 #include "queue.hpp"
 #include "segment.hpp"
 #include "paging.hpp"
+#include "memory_manager.hpp"
 
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
@@ -38,6 +39,9 @@ PixelWriter* pixel_writer;
 
 char console_buf[sizeof(Console)]; // Consoleのインスタンスを配置する領域
 Console* console;
+
+char memory_manager_buf[sizeof(BitmapMemoryManager)];
+BitmapMemoryManager* memory_manager;
 
 int printk(const char* format, ...) {
   va_list ap;
@@ -150,20 +154,31 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
 
   SetupIdentityPageTable();
 
+  ::memory_manager = new(memory_manager_buf) BitmapMemoryManager;
+
   const auto memory_map_base = reinterpret_cast<uintptr_t>(memory_map.buffer);
+  uintptr_t available_end = 0;
   for (uintptr_t iter = memory_map_base;
        iter < memory_map_base + memory_map.map_size;
        iter += memory_map.descriptor_size) {
     auto desc = reinterpret_cast<MemoryDescriptor*>(iter);
+    if (available_end < desc->physical_start) {
+      memory_manager->MarkAllocated(
+          FrameID(available_end / kBytesPerFrame),
+          (desc->physical_start - available_end) / kBytesPerFrame);
+    }
+
+    const auto physical_end =
+      desc->physical_start + desc->number_of_pages * kUEFIPageSize;
     if (IsAvailable(static_cast<MemoryType>(desc->type))) {
-      printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
-          desc->type,
-          desc->physical_start,
-          desc->physical_start + (desc->number_of_pages * 4096) - 1,
-          desc->number_of_pages,
-          desc->attribute);
+      available_end = physical_end;
+    } else {
+      memory_manager->MarkAllocated(
+          FrameID(desc->physical_start / kBytesPerFrame),
+          (physical_end - desc->physical_start) / kBytesPerFrame);
     }
   }
+  memory_manager->SetMemoryRange(FrameID(1), FrameID(available_end / kBytesPerFrame));
 
   mouse_cursor = new(mouse_cursor_buf) MouseCursor{
     *pixel_writer, kDesktopBGColor, {300, 200}
--- kernel/memory_map.hpp
+++ kernel/memory_map.hpp
@@ -53,4 +53,6 @@ inline bool IsAvailable(MemoryType memory_type) {
     memory_type == MemoryType::kEfiBootServicesData ||
     memory_type == MemoryType::kEfiConventionalMemory;
 }
+
+const int kUEFIPageSize = 4096;
 #endif /* __cplusplus */
