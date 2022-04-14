367b6ad6a62c9a9d898a8f23cb3ab8768e80d9f9
Hiroshi Hamada <>
2022-04-14 23:12:17 +0900

    Add Sleep(), Wakeup() at TaskManager.

--- kernel/task.hpp
+++ kernel/task.hpp
@@ -8,6 +8,9 @@
 #include <cstddef>
 #include <cstdint>
 #include <vector>
+#include <deque>
+
+#include "error.hpp"
 
 struct TaskContext {
   uint64_t cr3, rip, rflags, reserved1;            // offset 0x00
@@ -26,6 +29,9 @@ class Task {
     Task(uint64_t id);
     Task& InitContext(TaskFunc* f, int64_t data);
     TaskContext& Context();
+    uint64_t ID() const;
+    Task& Sleep();
+    Task& Wakeup();
 
   private:
     uint64_t id_;
@@ -37,12 +43,17 @@ class TaskManager {
   public:
     TaskManager();
     Task& NewTask();
-    void SwitchTask();
+    void SwitchTask(bool current_sleep = false);
+
+    void Sleep(Task* task);
+    Error Sleep(uint64_t id);
+    void Wakeup(Task* task);
+    Error Wakeup(uint64_t id);
 
   private:
     std::vector<std::unique_ptr<Task>> tasks_{};
+    std::deque<Task*> running_{};
     uint64_t latest_id_{0};
-    size_t current_task_index_{0};
 };
 
 extern TaskManager* task_manager;
