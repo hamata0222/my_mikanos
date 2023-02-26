f8657447d4458808fc66aa3126882d221694039d
Hiroshi Hamada <>
2023-02-26 12:51:31 +0900

    os_book_day15cまで写経

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -35,6 +35,7 @@
 #include "acpi.hpp"
 #include "keyboard.hpp"
 #include "task.hpp"
+#include "terminal.hpp"
 
 int printk(const char* format, ...) {
   va_list ap;
@@ -215,6 +216,10 @@ extern "C" void KernelMainNewStack(
                                 .InitContext(TaskB, 45) //
                                 .Wakeup()               //
                                 .ID();
+  const uint64_t task_terminal_id = task_manager->NewTask()
+                                        .InitContext(TaskTerminal, 0) //
+                                        .Wakeup()                     //
+                                        .ID();
 
   usb::xhci::Initialize();
   InitializeKeyboard();
@@ -254,10 +259,13 @@ extern "C" void KernelMainNewStack(
           timer_manager->AddTimer(
               Timer{msg->arg.timer.timeout + kTimer05sec, kTextboxCursorTimer});
           __asm__("sti");
-
           textbox_cursor_visible = !textbox_cursor_visible;
           DrawTextCursor(textbox_cursor_visible);
           layer_manager->Draw(text_window_layer_id);
+
+          __asm__("cli");
+          task_manager->SendMessage(task_terminal_id, *msg);
+          __asm__("sti");
         }
         break;
       case Message::kKeyPush:
